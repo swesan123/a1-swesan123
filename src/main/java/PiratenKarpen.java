ca4d5d69ac98fce497f2dbf6b8ac53bda07a97e3
Swesan Pathmanathan <pathmans@mcmaster.ca>
2023-01-26 19:41:59 -0500

    Started feature F04

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,6 +1,7 @@
 import pk.Dice;
 import pk.Faces;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PiratenKarpen {
@@ -8,10 +9,28 @@ public class PiratenKarpen {
 
     public static void main(String[] args) {
 
+
         int numGames = Integer.parseInt(args[0]);
+        int numDice = 8;
+
+        Dice myDice = new Dice(numDice-1);
+
+
+        while (numGames > 0) {
+
+            ArrayList<Faces> player1 = myDice.rollEight();
+            ArrayList<Faces> player2 = myDice.rollEight();
+
+            myDice.rollEight();
+            for (Faces face: player1) {
+
+            }
+
+            numGames--;
+        }
         System.out.println("Welcome to Piraten Karpen Simulator!");
         System.out.println("I'm rolling a dice");
-        Dice myDice = new Dice(7);
+
         System.out.println("  (DEBUG) there are " + myDice.NUMBER_FACES + " faces");
         System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
         System.out.println(myDice.roll());
