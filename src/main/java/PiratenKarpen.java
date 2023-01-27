ce80f71a39ded5227209561adb8084125c02d6e4
Swesan Pathmanathan <pathmans@mcmaster.ca>
2023-01-26 19:32:20 -0500

    Added F03 feature

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -7,6 +7,8 @@ public class PiratenKarpen {
 
 
     public static void main(String[] args) {
+
+        int numGames = Integer.parseInt(args[0]);
         System.out.println("Welcome to Piraten Karpen Simulator!");
         System.out.println("I'm rolling a dice");
         Dice myDice = new Dice(7);
@@ -18,8 +20,6 @@ public class PiratenKarpen {
         System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
         System.out.println(myDice.rollEight());
         System.out.println("That's all folks!");
-        System.out.println("test");
-        System.out.println("test");
 
     }
     
